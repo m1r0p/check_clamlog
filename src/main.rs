371915be275613c7c61425b5479c10e8b8e6252f
Igor M <eyegor.m@gmail.com>
2022-02-08 15:38:34 +0200

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
-    println!("aaa")
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+            panic!("not enough arguments");
+    }
+    if args.len() > 2 {
+            panic!("too much arguments");
+    }
+
+    let contents = fs::read_to_string(&args[1]).expect("Something wrong");
+
+    println!("{}", contents);
 }
