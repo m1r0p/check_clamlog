a2b7aa53178ee6e32742c2c068c06986dad0829f
Igor M <eyegor.m@gmail.com>
2022-02-08 16:26:46 +0200

    upd

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,20 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-            panic!("not enough arguments");
+        panic!("not enough arguments");
     }
     if args.len() > 2 {
-            panic!("too much arguments");
+        panic!("too much arguments");
     }
     let contents = fs::read_to_string(&args[1]).expect("Something wrong");
 
+    let mut infected: Vec<&str> = Vec::new();
     for line in contents.lines() {
         if line.contains("FOUND") {
-            println!("{}", line)
+            //println!("{}", &line);
+            infected.push(&line);
         }
     }
+
+    println!("{:?}", infected)
 }
