7af210e93933a53061301c669be93a307ffbb77f
Igor M <eyegor.m@gmail.com>
2022-02-11 16:28:24 +0200

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::env;
-use std::fs;
-use std::process;
+//use std::env;
+//use std::fs;
+//use std::process;
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
